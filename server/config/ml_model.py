de63faa1b1438f8dc158c52b5097fcd3a262f383
Zohaib Ali <zohaibaliwork@gmail.com>
2026-06-26 15:55:09 +0500

    production ready changes done in both frontend and backend

--- server/config/ml_model.py
+++ server/config/ml_model.py
@@ -1,11 +1,10 @@
-# config/ml_model.py
-
+from pathlib import Path
 import joblib
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+MODEL_PATH = BASE_DIR / "model" / "prediction_model.pkl"
+
 print("Loading ML Model...")
-
-model = joblib.load(
-    "model/prediction_model.pkl"
-)
-
+model = joblib.load(MODEL_PATH)
 print("Model Loaded Successfully")
--- server/main.py
+++ server/main.py
@@ -3,14 +3,25 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from routes.prediction_route import router
 
+import os
+
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+
+frontend = os.getenv("FRONTEND_URL")
+
+if frontend:
+    origins.append(frontend)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 app.include_router(router)

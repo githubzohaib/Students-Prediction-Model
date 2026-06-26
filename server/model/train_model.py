02b9be912a790dfff01bf418133e60e9790f057c
Zohaib Ali <zohaibaliwork@gmail.com>
2026-06-26 05:31:03 +0500

    Train model updated to generate low MB file of pkl

--- server/model/train_model.py
+++ server/model/train_model.py
@@ -33,10 +33,13 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 model = RandomForestRegressor(
-    n_estimators=100,
-    random_state=42
+    n_estimators=30,
+    max_depth=10,
+    min_samples_split=10,
+    min_samples_leaf=5,
+    random_state=42,
+    n_jobs=-1
 )
-
 model.fit(X_train, y_train)
 
 predictions = model.predict(X_test)

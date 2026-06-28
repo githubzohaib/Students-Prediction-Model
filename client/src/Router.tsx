6ea8077bd807776048c7fbfd47533fe534ae3a73
Zohaib Ali <zohaibaliwork@gmail.com>
2026-06-28 19:08:31 +0500

    build completetd

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,17 +1,17 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
-import Applayout from "./components/layouts/AppLayout";
+// import Applayout from "./components/layouts/AppLayout";
 import NoMatch from "./pages/NoMatch";
-import Error500 from "./pages/Error500";
+// import Error500 from "./pages/Error500";
 
 export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Applayout/>,
-  },
-  {
-    path: "/error", // ✅ NEW
-    element: <Error500 />,
-  },
+//   {
+//     path: "/",
+//     element: <Applayout/>,
+//   },
+//   {
+//     path: "/error", // ✅ NEW
+//     element: <Error500 />,
+//   },
   {
     path: "*",
     element: <NoMatch />,
